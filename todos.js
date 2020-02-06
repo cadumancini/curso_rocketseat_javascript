bf33d33ff6fff3de37abf8e4ecc906e36ef46e0f
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-02-06 16:59:04 -0300

    Iniciando aplicação

--- todos.js
+++ todos.js
@@ -1,1 +1,9 @@
-alert('Funcionou!');
+var listElement = document.querySelector("#app ul"); // de dentro de #app, pega a ul
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+  'Fazer café',
+  'Estudar Javascript',
+  'Acessar comunidade da Rocketseat'
+];
